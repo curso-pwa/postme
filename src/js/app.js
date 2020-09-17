d0331591e644d2a885e11a2c4d5a1b4af9ecbde8
adrian <adry.dabp@gmail.com>
2020-09-17 16:09:13 -0400

    este es mi ultimo cambio

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
-
+let deferredPrompt;
 // Funciones
 const showPostModal = () => {
   MAIN.style.display = 'none';
@@ -16,6 +16,11 @@ const closePostModal = () => {
   MAIN.style.display = 'block';
   MODAL_POST.style.transform = 'translateY(100vh)';
 };
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+});
 
 // Cuando se cargue todo nuestro DOM
 window.addEventListener('load', async () => {
@@ -32,5 +37,17 @@ window.addEventListener('load', async () => {
       console.info('Service worker registrado');
     }
   }
+
+  const bannerInstall = document.querySelector('#banner-install');
+  bannerInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const response = await deferredPrompt.userChoice;
+      if (response.outcome === 'dismissed') {
+        console.error('El usuario cancelo la instalación');
+      }
+    }
+  });
+
 
 });
