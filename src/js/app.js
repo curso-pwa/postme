9cfca7cea7d1fea2f493cc1e2e2c74f2678fb8b4
adrian <adry.dabp@gmail.com>
2020-09-17 15:46:41 -0400

    Inicio de una aplicacion progresiva

--- src/js/app.js
+++ src/js/app.js
@@ -18,11 +18,19 @@ const closePostModal = () => {
 };
 
 // Cuando se cargue todo nuestro DOM
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   MAIN = document.querySelector('#main');
   MODAL_POST = document.querySelector('#modal-post-section');
   BTN_SHOW_POST = document.querySelector('#btn-upload-post');
   BTN_SHOW_POST.addEventListener('click', showPostModal);
   BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
-  BTN_CANCEL_POST.addEventListener('click', closePostModal)
+  BTN_CANCEL_POST.addEventListener('click', closePostModal);
+
+  if ('serviceWorker' in navigator) {
+    const response = await navigator.serviceWorker.register('sw.js');
+    if (response) {
+      console.info('Service worker registrado');
+    }
+  }
+
 });
